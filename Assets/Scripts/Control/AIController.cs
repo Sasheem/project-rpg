e50dc83805c91781ea8bec6c3a9f0e48410587f6
Sasheem <sasheem.snell@gmail.com>
2023-11-08 15:41:00 -0500

    AI guarding and suspicion behavior

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -2,19 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using RPG.Combat;
 using RPG.Core;
+using RPG.Movement;
 using Unity.VisualScripting;
 using UnityEngine;
 
 namespace RPG.Control {
     public class AIController : MonoBehaviour {
         [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime = 5f;
+
         Fighter fighter;
         Health health;
+        Mover mover;
         GameObject player;
+
+        // state
+        Vector3 guardPosition;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
+
         private void Start() {
             fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
             player = GameObject.FindWithTag("Player");
+
+            guardPosition = transform.position;
         }
 
         // Attack player if within range, stop attacking if not
@@ -22,11 +34,39 @@ namespace RPG.Control {
             // cheeck if player is dead
             if (health.IsDead()) return;
 
-            if (InAttackRangeOfPlayer() && fighter.CanAttack(player)) {
-                fighter.Attack(player);
-            } else {
-                fighter.Cancel();
+            if (InAttackRangeOfPlayer() && fighter.CanAttack(player))
+            {
+                timeSinceLastSawPlayer = 0;
+                AttackBehavior();
             }
+            else if (timeSinceLastSawPlayer < suspicionTime)
+            {
+                SuspicionBehavior();
+            }
+            else
+            {
+                GuardBehavior();
+            }
+
+            // incrememnt by amount the frame took on every frame
+            timeSinceLastSawPlayer += Time.deltaTime;
+        }
+
+        // go back to guard position 
+        // will also cancel the fighting action
+        private void GuardBehavior()
+        {
+            mover.StartMoveAction(guardPosition);
+        }
+
+        private void SuspicionBehavior()
+        {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        private void AttackBehavior()
+        {
+            fighter.Attack(player);
         }
 
         // calculate the distance to the player
