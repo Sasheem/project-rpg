652ad14440bad20d7ee00e2187a667ee35c0e2f1
Sasheem <sasheem.snell@gmail.com>
2024-01-23 16:40:37 -0500

    Code documentation

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -7,21 +7,23 @@ namespace RPG.Combat {
     public class WeaponPickup : MonoBehaviour {
         [SerializeField] Weapon weapon = null;
         [SerializeField] float respawnTime = 5f;
-
+        
+        // Equip this weapon if player enters collider
         private void OnTriggerEnter(Collider other) {
             if (other.gameObject.tag == "Player") {
                 other.GetComponent<Fighter>().EquipWeapon(weapon);
                 StartCoroutine(HideForSeconds(respawnTime));
-                // Destroy(gameObject);
             }
         }
 
+        // Respawn weapon pickup after x seconds
         private IEnumerator HideForSeconds(float seconds) {
             ShowPickup(false);
             yield return new WaitForSeconds(seconds);
             ShowPickup(true);
         }
 
+        // Show or hide weapon pickup game objects
         private void ShowPickup(bool shouldShow) {
             GetComponent<Collider>().enabled = shouldShow;
             foreach (Transform child in transform) {
